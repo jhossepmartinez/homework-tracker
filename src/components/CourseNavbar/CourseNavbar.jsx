9d6e8e6af209f705cef318c35b53125ad48a35e7
jhossepmartinez <jhossepmv@gmail.com>
2024-01-23 19:48:47 -0300

    added shadow to course nabar

--- src/components/CourseNavbar/CourseNavbar.jsx
+++ src/components/CourseNavbar/CourseNavbar.jsx
@@ -5,15 +5,17 @@ import courseEntries from "../../courses.json"
 export default function CourseNavbar({ selectedCourse, onSelectCourse }) {
     return (
         <div className="container-snap flex space-x-2 text-sm overflow-x-auto">
-            {courseEntries.map(course => (
-                <button
-                    key={course.id}
-                    className="bg-slate-300 rounded-l-full rounded-r-full px-5 pt-1 pb-1 h-10 whitespace-nowrap"
-                    onClick={() => onSelectCourse(course.name)}
-                >
-                    {course.name}
-                </button>
-            ))}
+            {courseEntries.map(course => {
+                return (
+                    <button
+                        key={course.id}
+                        className="bg-slate-200 shadow-md mt-2 mb-2 rounded-l-full rounded-r-full px-5 pt-1 pb-1 h-10 whitespace-nowrap"
+                        onClick={() => onSelectCourse(course.name)}
+                    >
+                        {course.name}
+                    </button>
+                )
+            })}
         </div>
     )
 }
