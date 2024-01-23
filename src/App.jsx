8875231641913c117e0f0f7cc7b34b8a6845b893
jhossepmartinez <jhossepmv@gmail.com>
2024-01-23 18:51:50 -0300

    centered in wider screens

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import CourseNavbar from './components/CourseNavbar/CourseNavbar'
 import Tasks from './components/Tasks/Tasks'
+import CreateTaskButton from "./components/CreateTaskButton/CreateTaskButton"
 
 function App() {
     const [selectedCourse, setSelectedCourse] = useState("All");
@@ -9,11 +10,23 @@ function App() {
     const handleSelectCourse = (course) => {
         setSelectedCourse(course);
     }
+
     return (
-        <div className="mt-2 mx-4 font-inter">
-            <CourseNavbar selectedCourse={selectedCourse} onSelectCourse={handleSelectCourse} />
-            <div className="mt-2">
-                <Tasks selectedCourse={selectedCourse}/>
+        <div className="flex justify-center">
+            <div className="flex-col justify-center mt-2 mx-4 font-inter lg:w-[420px] lg:h-[667px] overflow-auto">
+                <div>
+                    <CourseNavbar selectedCourse={selectedCourse} onSelectCourse={handleSelectCourse} />
+                </div>
+                <div className="mt-2">
+                    <Tasks selectedCourse={selectedCourse}/>
+                </div>
+                <div className="relative">
+                    <div className="absolute">
+                        <div className="fixed bottom-10 right-4">
+                            <CreateTaskButton />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
