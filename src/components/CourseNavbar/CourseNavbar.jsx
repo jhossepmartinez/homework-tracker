df2dc8f47062e85bc685c5f0511d805de196cebd
jhossepmartinez <jhossepmv@gmail.com>
2024-01-23 18:51:05 -0300

    compacted course navbar with map

--- src/components/CourseNavbar/CourseNavbar.jsx
+++ src/components/CourseNavbar/CourseNavbar.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
+import "./CourseNavbar.css"
+import courseEntries from "../../courses.json"
 
 export default function CourseNavbar({ selectedCourse, onSelectCourse }) {
     return (
-        <div className="flex space-x-2 text-xs">
-            <button
-                className="bg-gray-300 rounded-l-full rounded-r-full px-5 pt-1 pb-1"
-                onClick={() => onSelectCourse("All")}
-            >
-                All
-            </button>
-            <button
-                className="bg-gray-300 rounded-l-full rounded-r-full px-5 pt-1 pb-1"
-                onClick={() => onSelectCourse("Physics")}
-            >
-                Physics
-            </button>
-            <button
-                className="bg-gray-300 rounded-l-full rounded-r-full px-5 pt-1 pb-1"
-                onClick={() => onSelectCourse("Math")}
-            >
-                Math
-            </button>
+        <div className="container-snap flex space-x-2 text-sm overflow-x-auto">
+            {courseEntries.map(course => (
+                <button
+                    key={course.id}
+                    className="bg-slate-300 rounded-l-full rounded-r-full px-5 pt-1 pb-1 h-10 whitespace-nowrap"
+                    onClick={() => onSelectCourse(course.name)}
+                >
+                    {course.name}
+                </button>
+            ))}
         </div>
     )
 }
